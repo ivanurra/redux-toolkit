5c5dc624cc063fa1fb751fc74b805b48acfc2c14
Ivan Urra <ivan.urra.wd@gmail.com>
2023-01-05 12:25:40 +0100

    WIP

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import { useDispatch, useSelector } from "react-redux";
+import { increment, decrement, incrementBy } from "./store/slices/counter";
+
 function App() {
+  // const { counter } = useSelector( state => state.counter )
+  const dispatch = useDispatch();
+
   return (
     <div>
-      <h1>Hello World</h1>
+      <header>
+        {/* <p>count is: { counter }</p> */}
+        <p>
+          <button type="button" onClick={() => dispatch(increment())}>
+            Increment
+          </button>
+          <button type="button" onClick={() => dispatch(decrement())}>
+            Decrement
+          </button>
+          <button type="button" onClick={() => dispatch(incrementBy(2))}>
+            Increment by 2
+          </button>
+        </p>
+      </header>
     </div>
   );
 }
