ab725429b397b0724f36848d0498735430c800b0
Ivan Urra <ivan.urra.wd@gmail.com>
2023-01-08 12:22:23 +0100

    WIP

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+// import App from "./App";
 import { store } from "./store";
 import { Provider } from "react-redux";
+import { PokemonApp } from "./store/PokemonApp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      {/* <App /> */}
+      <PokemonApp />
     </Provider>
   </React.StrictMode>
 );
---
+++ src/store/PokemonApp.jsx
@@ -0,0 +1,13 @@
+export const PokemonApp = () => {
+  return (
+    <>
+      <h1>PokemonApp</h1>
+      <hr />
+      <ul>
+        <li>XXX</li>
+        <li>XXX</li>
+        <li>XXX</li>
+      </ul>
+    </>
+  );
+};
