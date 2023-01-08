5b29cd251f5cabbb32a55c14969ad00ffbd11b2b
Ivan Urra <ivan.urra.wd@gmail.com>
2023-01-08 12:25:33 +0100

    WIP

--- src/store/slices/pokemon/pokemonSlice.js
+++ src/store/slices/pokemon/pokemonSlice.js
@@ -3,19 +3,15 @@ import { createSlice } from "@reduxjs/toolkit";
 export const pokemonSlice = createSlice({
   name: "pokemon",
   initialState: {
-    counter: 10,
+    page: 0,
+    pokemons: [],
+    isLoading: false,
   },
   reducers: {
     increment: (state /* action */) => {
-      //! https://react-redux.js.org/tutorials/quick-start
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
       state.counter += 1;
     },
   },
 });
 
-// Action creators are generated for each case reducer function
 export const { increment } = pokemonSlice.actions;
