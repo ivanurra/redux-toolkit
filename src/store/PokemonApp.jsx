ac9fafe00c800a5296fe83fe91af6dbb116584ec
Ivan Urra <ivan.urra.wd@gmail.com>
2023-01-09 08:17:42 +0100

    WIP

--- src/store/PokemonApp.jsx
+++ src/store/PokemonApp.jsx
@@ -1,4 +1,14 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { getPokemons } from "./slices/pokemon";
+
 export const PokemonApp = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPokemons());
+  }, []);
+
   return (
     <>
       <h1>PokemonApp</h1>
---
+++ src/store/slices/pokemon/thunks.js
@@ -0,0 +1,10 @@
+import { setPokemons, startLoadingPokemons } from "./pokemonSlice";
+
+export const getPokemons = (page = 0) => {
+  return async (dispatch, getState) => {
+    dispatch(startLoadingPokemons());
+    // HTTP CALL
+
+    // Dispatch setPokemons
+  };
+};
