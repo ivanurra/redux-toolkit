da674e89028a1bfb41091916ee455b5319c7badf
Ivan Urra <ivan.urra.wd@gmail.com>
2023-01-12 08:12:38 +0100

    WIP

---
+++ src/TodoApp.jsx
@@ -0,0 +1,11 @@
+export const TodoApp = () => {
+  return (
+    <>
+      <h1>ToDo - RTK Query</h1>
+      <hr />
+      <h4>isLoading...</h4>
+      <pre>...</pre>
+      <button>Next ToDo</button>
+    </>
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,15 @@ import ReactDOM from "react-dom/client";
 // import App from "./App";
 import { store } from "./store";
 import { Provider } from "react-redux";
-import { PokemonApp } from "./store/PokemonApp";
+import { TodoApp } from "./TodoApp";
+// import { PokemonApp } from "./store/PokemonApp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       {/* <App /> */}
-      <PokemonApp />
+      {/* <PokemonApp /> */}
+      <TodoApp />
     </Provider>
   </React.StrictMode>
 );
